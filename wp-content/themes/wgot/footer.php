9567b429f5f7ab2e88d2b7de2f7c1a6028aa57b8
Ed Porras <technical@wgot.org>
2017-05-14 08:50:10 -0700

    updated facebook link & removed last.fm

--- wp-content/themes/wgot/footer.php
+++ wp-content/themes/wgot/footer.php
@@ -3,8 +3,7 @@
 <p><strong><?php bloginfo('name');?></strong> is a 501[c][3] Non-profit. 
 Contact: <a href="mailto:info@wgot.org">info@wgot.org</a>.
 <a href="http://www.twitter.com/wgotlp"><img src="http://twitter-badges.s3.amazonaws.com/t_mini-a.png" /></a> | 
-<a href="http://www.facebook.com/pages/Gainesville-FL/WGOT-LP-947-FM-Gainesville-FL/55772315845"><img src="/wp-content/themes/wgot/img/facebook_16.png" /></a> | 
-<a href="http://www.last.fm/group/WGOT-LP+94.7+FM"><img src="/wp-content/themes/wgot/img/lastfm-16.png" /></a> | 
+<a href="https://www.facebook.com/wgotlp/"><img src="/wp-content/themes/wgot/img/facebook_16.png" /></a> | 
 <a href="<?php bloginfo('rss2_url'); ?>"><img src="/wp-content/themes/wgot/img/icon_feed.gif" /></a> | 
 <a href="/wp-login.php/">User login</a>
 </p>
