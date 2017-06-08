cd8744ef2086b549d2c7f0fea713b3f98d6a120b
WGOT Technical <technical@wgot.org>
2017-06-08 14:42:26 -0400

    remove fib about 501(3)c status

--- wp-content/themes/wgot/footer.php
+++ wp-content/themes/wgot/footer.php
@@ -1,9 +1,8 @@
 
 <div id="footer">
-<p><strong><?php bloginfo('name');?></strong> is a 501[c][3] Non-profit. 
-Contact: <a href="mailto:info@wgot.org">info@wgot.org</a>.
-<a href="http://www.twitter.com/wgotlp"><img src="http://twitter-badges.s3.amazonaws.com/t_mini-a.png" /></a> | 
+<p>Follow <strong><?php bloginfo('name');?></strong> on social media: 
 <a href="https://www.facebook.com/wgotlp/"><img src="/wp-content/themes/wgot/img/facebook_16.png" /></a> | 
+<a href="http://www.twitter.com/wgotlp"><img src="http://twitter-badges.s3.amazonaws.com/t_mini-a.png" /></a> | 
 <a href="<?php bloginfo('rss2_url'); ?>"><img src="/wp-content/themes/wgot/img/icon_feed.gif" /></a> | 
 <a href="/wp-login.php/">User login</a>
 </p>
